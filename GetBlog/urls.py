0b06f79fd3e03cc62543330f8e293e15db9bfb19
parsa <Mohammadparsa1384@users.noreply.github.com>
2025-04-11 16:42:43 +0330

    Login logout button for test

--- GetBlog/urls.py
+++ GetBlog/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path("", TemplateView.as_view("index.html"))
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
